ac9b59a491d929553c00b8077626242e358ce15c
Valentin Deguilhem <valentindeguilhem@gmail.com>
2022-02-17 18:29:30 +0100

    Added simple dash.

--- Assets/External/StarterAssets/Mobile/Scripts/CanvasInputs/UICanvasControllerInput.cs
+++ Assets/External/StarterAssets/Mobile/Scripts/CanvasInputs/UICanvasControllerInput.cs
@@ -27,6 +27,11 @@ namespace StarterAssets
         {
             starterAssetsInputs.SprintInput(virtualSprintState);
         }
+
+        public void VirtualDashInput(bool virtualDashState)
+        {
+            starterAssetsInputs.DashInput(virtualDashState);
+        }
         
     }
 
